53790331b1c730c4c24c37ae22b53a5a71fa1e26
kborling <kborling@gmail.com>
2014-04-27 21:27:17 -0700

    First commit of Plante's Ferry mini game

---
+++ SpokaneValleyApp/src/com/spokanevalley/app/PlantesFerry.java
@@ -0,0 +1,52 @@
+/*
+ * @author Kevin Borling
+ * Plante's Ferry mini game
+ */
+package com.spokanevalley.app;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class PlantesFerry implements ApplicationListener{
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+}// End class

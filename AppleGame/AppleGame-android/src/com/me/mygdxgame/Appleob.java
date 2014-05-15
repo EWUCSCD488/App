3698d461f47a0dbabd67542c2fb5fdba67bbac8d
ewu-shovelierp <pshovelier@eagles.ewu.edu>
2014-05-15 14:35:50 -0700

    APPLES

---
+++ AppleGame/AppleGame-android/src/com/me/mygdxgame/Appleob.java
@@ -0,0 +1,17 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Appleob 
+{
+	public Texture image;
+	public Rectangle rec;
+	public int points = 1;
+	
+	Appleob()
+	{
+		rec = new Rectangle();
+	}
+	
+}
---
+++ AppleGame/AppleGame-android/src/com/me/mygdxgame/Constants.java
@@ -0,0 +1,15 @@
+package com.me.mygdxgame;
+
+public class Constants {
+	public static final String IMAGE_BUCKET = "bucket.png";
+	public static final String IMAGE_APPLE = "apple.png";
+	public static final String IMAGE_GAPPLE = "goldapple.png";
+	public static final String IMAGE_RAPPLE = "rotapple.png";
+	public static final String BACKGROUND = "tree.png";
+	
+	public static final String SOUND_MUSIC = "BadApple.mp3";
+	public static final String SOUND_APPLE = "basket.wav";
+	
+	public static final int HEIGHT_GAME = 800;
+	public static final int WIDTH_GAME = 480;
+}

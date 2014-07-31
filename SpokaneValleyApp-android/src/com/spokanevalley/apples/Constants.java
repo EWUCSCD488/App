ee7b07d966cd7df37f123327202423ca5b1c6c99
ewu-shovelierp <pshovelier@eagles.ewu.edu>
2014-07-31 15:12:18 -0700

    Put Ski and Apple assets into separate folders

--- SpokaneValleyApp-android/src/com/spokanevalley/apples/Constants.java
+++ SpokaneValleyApp-android/src/com/spokanevalley/apples/Constants.java
@@ -1,18 +1,18 @@
 package com.spokanevalley.apples;
 
 public class Constants {
-	public static final String IMAGE_BUTTON = "gfx/button.png";
-	public static final String IMAGE_BUCKET = "gfx/bucket.png";
-	public static final String IMAGE_APPLE = "gfx/apple.png";
-	public static final String IMAGE_GAPPLE = "gfx/goldapple.png";
-	public static final String IMAGE_RAPPLE = "gfx/rotapple.png";
-	public static final String BACKGROUND = "gfx/appletreenew.png";
+	public static final String IMAGE_BUTTON = "appleAsssets/gfx/button.png";
+	public static final String IMAGE_BUCKET = "appleAsssets/gfx/bucket.png";
+	public static final String IMAGE_APPLE = "appleAsssets/gfx/apple.png";
+	public static final String IMAGE_GAPPLE = "appleAsssets/gfx/goldapple.png";
+	public static final String IMAGE_RAPPLE = "appleAsssets/gfx/rotapple.png";
+	public static final String BACKGROUND = "appleAsssets/gfx/appletreenew.png";
 	
 	public static final String GAME_FONT = "fonts/gamefont.fnt";
 	public static final String MENU_FONT = "fonts/myriad.fnt";
 	
-	public static final String SOUND_MUSIC = "sfx/BadApple.mp3";
-	public static final String SOUND_APPLE = "sfx/basket.wav";
+	public static final String SOUND_MUSIC = "appleAsssets/sfx/BadApple.mp3";
+	public static final String SOUND_APPLE = "appleAsssets/sfx/basket.wav";
 	
 	public static final int HEIGHT_GAME = 800;
 	public static final int WIDTH_GAME = 480;
--- SpokaneValleyApp-android/src/com/spokanevalley/ski/Constants.java
+++ SpokaneValleyApp-android/src/com/spokanevalley/ski/Constants.java
@@ -1,28 +1,28 @@
 package com.spokanevalley.ski;
 //package com.me.mygdxgame;
 public class Constants {
-	public static final String HOWTO_SKI = "gfx/Skihow.png";
-	public static final String IMAGE_BUTTON = "gfx/button.png";
-	public static final String IMAGE_BUTTON2 = "gfx/button2.png";
-	public static final String IMAGE_BUTTON3= "gfx/button3.png";
-	public static final String IMAGE_BUTTONL = "gfx/buttonL.png";
-	public static final String IMAGE_BUTTONR = "gfx/buttonR.png";
-	public static final String IMAGE_SKIER = "gfx/Ski.png";
-	public static final String IMAGE_SKIERL = "gfx/SkiL.png";
-	public static final String IMAGE_SKIERR = "gfx/SkiR.png";
-	public static final String IMAGE_FLAG = "gfx/flags.png";
-	public static final String IMAGE_BFLAG = "gfx/bflags.png";
-	public static final String IMAGE_YFLAG = "gfx/goldflags.png";
-	public static final String IMAGE_PTREE = "gfx/skitree.png";
-	public static final String BACKGROUND = "gfx/snow.png";
+	public static final String HOWTO_SKI = "skiAssets/gfx/Skihow.png";
+	public static final String IMAGE_BUTTON = "skiAssets/gfx/button.png";
+	public static final String IMAGE_BUTTON2 = "skiAssets/gfx/button2.png";
+	public static final String IMAGE_BUTTON3= "skiAssets/gfx/button3.png";
+	public static final String IMAGE_BUTTONL = "skiAssets/gfx/buttonL.png";
+	public static final String IMAGE_BUTTONR = "skiAssets/gfx/buttonR.png";
+	public static final String IMAGE_SKIER = "skiAssets/gfx/Ski.png";
+	public static final String IMAGE_SKIERL = "skiAssets/gfx/SkiL.png";
+	public static final String IMAGE_SKIERR = "skiAssets/gfx/SkiR.png";
+	public static final String IMAGE_FLAG = "skiAssets/gfx/flags.png";
+	public static final String IMAGE_BFLAG = "skiAssets/gfx/bflags.png";
+	public static final String IMAGE_YFLAG = "skiAssets/gfx/goldflags.png";
+	public static final String IMAGE_PTREE = "skiAssets/gfx/skitree.png";
+	public static final String BACKGROUND = "skiAssets/gfx/snow.png";
 	
 	public static final String GAME_FONT = "fonts/gamefont.fnt";
 	public static final String MENU_FONT = "fonts/myriad.fnt";
 	
-	public static final String SOUND_MUSIC = "sfx/BadApple.mp3";
-	public static final String SOUND_TREE = "sfx/treeSound.mp3";
-	public static final String SOUND_FLAG = "sfx/ding.mp3";
-	public static final String SOUND_BFLAG = "sfx/bDing.mp3";
+	public static final String SOUND_MUSIC = "skiAssets/sfx/BadApple.mp3";
+	public static final String SOUND_TREE = "skiAssets/sfx/treeSound.mp3";
+	public static final String SOUND_FLAG = "skiAssets/sfx/ding.mp3";
+	public static final String SOUND_BFLAG = "skiAssets/sfx/bDing.mp3";
 	
 	public static final int HEIGHT_GAME = 480;
 	public static final int WIDTH_GAME = 800;

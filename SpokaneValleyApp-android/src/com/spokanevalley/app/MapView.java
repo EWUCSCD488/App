f18e1f13920a1e449f1788fa42ce32cc4b152c10
kborling <kborling@gmail.com>
2014-08-03 21:22:44 -0700

    Updated the Main Map Graphic. Added roads, railroad, train, cars, and signs. Also updated the grass texture as well as the water texture. The image is now 4K resolution instead of 1080p.

--- SpokaneValleyApp-android/src/com/spokanevalley/app/MapView.java
+++ SpokaneValleyApp-android/src/com/spokanevalley/app/MapView.java
@@ -222,7 +222,7 @@ public class MapView extends Activity implements OnMarkerClickListener,
 		GroundOverlay groundOverlay = map
 				.addGroundOverlay(new GroundOverlayOptions()
 						.image(BitmapDescriptorFactory
-								.fromResource(R.drawable.newmapbg))
+								.fromResource(R.drawable.mapnew))
 						.position(new LatLng(47.670568, -117.239437), 39000)
 						.transparency(0.0f));
 
